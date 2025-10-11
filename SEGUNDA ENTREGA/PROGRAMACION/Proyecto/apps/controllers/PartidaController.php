0fa712049532f9ca9f50f58082b31f8186a08127
codeflow838 <codeflow838@gmail.com>
2025-10-11 05:34:20 -0300

    partidawitchmodo

--- SEGUNDA ENTREGA/PROGRAMACION/Proyecto/apps/controllers/PartidaController.php
+++ SEGUNDA ENTREGA/PROGRAMACION/Proyecto/apps/controllers/PartidaController.php
@@ -7,32 +7,33 @@ $db = new Database();
 $conn = $db->getConnection();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'crear_partida') {
-
+    
     $id_usuario = $_SESSION['user_id'];
     $modo = $_POST['modo'] ?? 'seguimiento';
     $fecha = date('Y-m-d');
     $cantidad_jugadores = intval($_POST['cantidad_jugadores'] ?? 2);
 
-    if ($modo === 'digital' || $modo === 'seguimiento') {
-        $partida = new Partida($id_usuario, $modo, $fecha, $cantidad_jugadores);
-        if ($partida->save($conn)) {
-
-            switch ($cantidad_jugadores) {
-                case 2:
-                    include '../views/juegocalculadora2.php';
-                    break;
-                case 3:
-                    include '../views/juegocalculadora3.php';
-                    break;
-                case 4:
-                    include '../views/juegocalculadora4.php';
-                    break;
-                case 5:
-                    include '../views/juegocalculadora5.php';
-                    break;
+    switch ($modo) {
+        case 'digital':
+        case 'seguimiento':
+            $partida = new Partida($id_usuario, $modo, $fecha, $cantidad_jugadores);
+            if ($partida->save($conn)) {
+                switch ($cantidad_jugadores) {
+                    case 2:
+                        include '../views/juegocalculadora2.php';
+                        break;
+                    case 3:
+                        include '../views/juegocalculadora3.php';
+                        break;
+                    case 4:
+                        include '../views/juegocalculadora4.php';
+                        break;
+                    case 5:
+                        include '../views/juegocalculadora5.php';
+                        break;
+                }
             }
-
-        }
+            break;
     }
 }
 ?>
